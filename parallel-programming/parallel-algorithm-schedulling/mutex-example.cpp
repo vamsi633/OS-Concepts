6f34015b24bdeaeccba3e009bac751a4d0ff6c83
vamsi <vamsikrishnanouluri@gmail.com>
2026-02-10 00:42:36 -0800

    commit

--- parallel-programming/parallel-algorithm-schedulling/mutex-example.cpp
+++ parallel-programming/parallel-algorithm-schedulling/mutex-example.cpp
@@ -6,72 +6,73 @@
 std::mutex sharedMutex;
 
 void doWorkWithMutexLock(int threadIndex){
-    sharedMutex.lock();
+    std::lock_guard<std::mutex> lock(sharedMutex);
     std::cout<<"Performing work for thread: "<<threadIndex <<"\n";
-    std::cout<<"Sleeping for 1 second\n";
+    std::cout<<"Thread "<<threadIndex<<" is sleeping for 1 second...\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    sharedMutex.unlock();
 }
 
 void executeThreadsWithMutexLock(int numThreads){
-    std::thread threads[numThreads];
-    std::cout<<"Starting "<<numThreads <<" threads to use lock with shared mutex\n";
+    std::vector<std::thread> threads;
+    std::cout<<"Starting Parallel Execution\n";
     int counter=0;
 
     for(int i=0;i<numThreads;i++){
-        threads[i]=std::thread(doWorkWithMutexLock,i);
-
+        threads.push_back(std::thread(doWorkWithMutexLock,i));
+    }
+        std::cout << "All " << numThreads << " threads have been launched and are running!\n";
         std::cout << "Press a key to let program proceed\n";
         getchar();
 
-        std::cout<<"Joining threads\n";
+        
         for(int i=0;i<numThreads;++i){
-            threads[i].join();
+            if (threads[i].joinable()) { // Safety check!
+            threads[i].join(); 
+            }
         }
-        std::cout<<"Press a key to let program proceed\n";
-        getchar();
-        std::cout<<"Completed all threads\n";
-    }
+        std::cout<<"Completed all threads safely\n";
 }
 
 void doWorkWithMutexTryLock(int threadIndex){
-    int counter=0;
-    while(!sharedMutex.try_lock()){
-        counter++;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    int attempts = 0;
+    // Keep trying to get the lock without blocking the whole program
+    while (!sharedMutex.try_lock()) {
+        attempts++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    std::cout<<"Counter for work for thread "<< threadIndex <<" is "<< counter<<"\n";
+    
+    std::cout << "Thread " << threadIndex << " got the lock after " << attempts << " failed attempts.\n";
     sharedMutex.unlock();
 }
 
 void executeAndDetachThreadsWithMutexTryLock(int numThreads){
-    std::thread threads[numThreads];
-    std::cout<<"Starting "<<numThreads <<" threads with try_lock on shared mutex\n";
+    std::vector<std::thread> threads;
+    std::cout << "\n--- STARTING TRY_LOCK DETACHED THREADS ---\n";
     
-    //spawn n threads
-    for(int i=0;i<numThreads;++i){
-        threads[i].detach();
+    // 1. Actually spawn the threads (Fixing the previous bug)
+    for (int i = 0; i < numThreads; ++i) {
+        threads.push_back(std::thread(doWorkWithMutexTryLock, i));
     }
-    std::cout<<"Press a key to let program proceed\n";
+
+    // 2. Detach them immediately
+    for (int i = 0; i < numThreads; ++i) {
+        if (threads[i].joinable()) {
+            threads[i].detach();
+        }
+    }
+    std::cout << "Threads are now running in the background. Press ENTER to finish.\n";
     getchar();
 
 
 }
 int main(int argc, char *argv[]){
-    int numThreads=3;
-    if(argc>1){
-        numThreads=atoi(argv[1]);
+    int numThreads = 3;
+    if (argc > 1) {
+        numThreads = atoi(argv[1]);
     }
 
-    std::cout << "Pay attention to the fact that newlines will not always be added at the end of lines if multiple <<'s\n";
-	executeThreadsWithMutexLock(numThreads);
-	
-	executeAndDetachThreadsWithMutexTryLock(numThreads);
+    executeThreadsWithMutexLock(numThreads);
+    executeAndDetachThreadsWithMutexTryLock(numThreads);
 
-	std::cout << "Sleeping for 1 second\n";
-	std::this_thread::sleep_for (std::chrono::seconds(1));
-	
-	std::cout << "Press a key to let program proceed\n";
-	getchar();
-	return 0;
+    return 0;
 }
